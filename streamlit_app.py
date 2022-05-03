0054c054048b2a9124d3f1009f920f2e6088b8aa
tampa1093 <104864679+tampa1093@users.noreply.github.com>
2022-05-03 15:55:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('breakfast menu')
-streeamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Omega 3 & blueberry oatmeal')
 streamlit.text('Kale,Spinash and rocket smoothie')
 streamlit.text('Hard boiled free range egg')
