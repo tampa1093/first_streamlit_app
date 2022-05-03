a56f60d32fe6d561a5e5094aaeb2c9feda3f041e
tampa1093 <104864679+tampa1093@users.noreply.github.com>
2022-05-03 15:54:50 -0400

    Update streamlit_app.py
    added options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+steamlit.header('breakfast menu')
+streeamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale,Spinash and rocket smoothie')
+streamlit.text('Hard boiled free range egg')
